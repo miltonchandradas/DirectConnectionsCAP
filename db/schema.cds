0ac48d6a3a924437f48aa5e02d73c9c639b87257
Milton Chandradas <milton.chandradas@live.com>
2020-06-28 07:22:19 +0000

    doc changes

--- db/schema.cds
+++ db/schema.cds
@@ -1,11 +1,17 @@
+namespace demo;
 
-namespace demo;
 type Address {
     street : String(100);
     city   : String(20);
     state  : String(10);
     zip    : String(10);
 }
+
+type mediumString50 : String(50);
+type longString100 : String(100);
+type veryLongString1000 : String(1000);
+type sDate : DateTime;
+type fourdigitDecimal : Decimal(15, 4);
 
 entity Products {
     key id          : Integer;
@@ -27,3 +33,27 @@ entity Suppliers {
         products    : Association to many Products
                           on products.supplier = $self;
 }
+
+
+entity User {
+    key id               : String(50);
+        firstName        : mediumString50 not null;
+        lastName         : mediumString50 not null;
+        email            : mediumString50 not null;
+        password         : longString100  not null default 'nopassword';
+        isFacebookUser   : Boolean        default false;
+        fbId             : mediumString50;
+        isAdmin          : Boolean        default false;
+        photoUrl         : longString100;
+        categoryId       : Integer;
+        karmaPoints      : Integer        default 100;
+        formattedAddress : longString100;
+        coordinates      : hana.ST_POINT(4326) null;
+        isochrone5mCar   : hana.ST_GEOMETRY(4326) null;
+}
+
+
+
+
+
+
--- srv/server.js
+++ srv/server.js
@@ -2,13 +2,74 @@
 "use strict";
 
 const express = require("express");
-const cds = require('@sap/cds')
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const colors = require("colors");
+const cors = require("cors");
+const errorHandler = require("./middleware/error");
+const xsHDBConn = require("@sap/hdbext");
+const xsenv = require("@sap/xsenv");
+const cds = require('@sap/cds');
+const webpush = require("web-push");
+
+
+// Load environment variables
+dotenv.config({ path: "./srv/config/config.env" });
+
+xsenv.loadEnv();
+let hanaOptions = xsenv.getServices({
+	hana: {
+		tag: "hana"
+	}
+});
+
+const auth = require("./routes/auth");
+const products = require("./routes/products");
+const push = require("./routes/push");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello from express"));
+// Set static path
+app.use(express.static(path.join(__dirname, "client")));
 
-cds.connect.to ('db');
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Enabling CORS for frontend browser request
+app.use(cors());
+
+// Logging
+app.use(morgan("dev"));
+
+// Web push details
+webpush.setVapidDetails(
+    "mailto:test@test.com",
+    process.env.PUBLIC_VAPID_KEY,
+    process.env.PRIVATE_VAPID_KEY
+);
+
+
+app.use(xsHDBConn.middleware(hanaOptions.hana));
+
+app.use("/api/v1/products", products);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/push", push);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+const nodeEnv = process.env.NODE_ENV || "dev";
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "docs", "index.html"));
+});
+
+cds.connect();
 cds.serve ('TechService').in(app);
 
-app.listen(5000);
+app.listen(PORT, console.log(`Server running in ${nodeEnv} mode on port ${PORT}`.yellow.inverse));
